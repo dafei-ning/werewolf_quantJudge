086d8ddf06a6581c5ccfd85e2c4d5ef21fa0c5ce
Dafei Ning <ningdafei@gmail.com>
2019-11-11 16:33:02 -0800

    refactor turninfo

---
+++ lib/2.playerBehavior/turnInfo.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:werewolf_quantjudge/2.playerBehavior/playerBehaviorRecordCard.dart';
+import './behavior.dart';
+
+class TurnInfo extends StatelessWidget {
+  final int id;
+  final int turn; // to be changed into turnselector object.
+
+  final List<Behavior> behaviors = [
+    Behavior(
+      id: 1,
+      player: 1,
+      turn: 2,
+      title: '坐姿状态',
+      quantity: 50,
+      date: DateTime.now(),
+    ),
+    Behavior(
+      id: 2,
+      player: 1,
+      title: '发言逻辑',
+      quantity: 30,
+      date: DateTime.now(),
+      turn: 1,
+    ),
+    Behavior(
+      id: 3,
+      player: 2,
+      title: '站边举票',
+      quantity: 20,
+      date: DateTime.now(),
+      turn: 1,
+    ),
+    Behavior(
+      id: 4,
+      player: 2,
+      title: '站边举票',
+      quantity: 20,
+      date: DateTime.now(),
+      turn: 2,
+    ),
+    Behavior(
+      id: 5,
+      player: 3,
+      title: '站边举票行为',
+      quantity: 20,
+      date: DateTime.now(),
+      turn: 3,
+    ),
+  ];
+
+  /*
+   * Constructor
+   */
+  TurnInfo(this.id, this.turn);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // // 2.1.1.1 behavior id
+        // Container(child: Text(id.toString())),
+        // 2.1.1.1 轮数
+        Container(
+          child: Card(
+            child: Text(
+              "第" + turn.toString() + "轮",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ),
+
+        // 每一轮的用户行为
+        Container(
+          child: Column(
+            children: behaviors.map((bh) {
+              return Container(child: playerBehaviorRecordCard(bh));
+            }).toList(),
+          ),
+        )
+      ],
+    );
+  }
+}
