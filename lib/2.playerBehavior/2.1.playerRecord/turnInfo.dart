eebf8acf1868770345169bbc8879398a1d33c68f
Dafei Ning <ningdafei@gmail.com>
2019-11-11 16:09:17 -0800

    adjust title font

--- lib/2.playerBehavior/2.1.playerRecord/turnInfo.dart
+++ lib/2.playerBehavior/2.1.playerRecord/turnInfo.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class TurnInfo extends StatelessWidget {
-
   final int id;
   final int turn; // to be changed into turnselector object.
 
@@ -15,9 +14,23 @@ class TurnInfo extends StatelessWidget {
     return Row(
       children: <Widget>[
         // 2.1.1.1 behavior id
-        Container(child: Text(id.toString())),
+        Container(
+          child: Text(
+            id.toString(),
+          ),
+        ),
         // 2.1.1.2 轮数
-        Container(child: Card(child: Text("第" + turn.toString() + "轮"))),
+        Container(
+          child: Card(
+            child: Text(
+              "第" + turn.toString() + "轮",
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+            ),
+          ),
+        ),
       ],
     );
   }
