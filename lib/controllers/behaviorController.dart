087975bc1419e3c36a0426e0d35e82952eb3e325
Dafei Ning <ningdafei@gmail.com>
2019-12-03 23:08:38 -0800

    groupedBehaviorValues for turnRecord

--- lib/controllers/behaviorController.dart
+++ lib/controllers/behaviorController.dart
@@ -3,10 +3,12 @@ import '../models/behavior.dart';
 import '../models/individual.dart';
 
 class BehaviorController {
-  
   // 将每一次记录的玩家Behavior Map成以轮次为主导的玩家behavior集合（mappedBehaviors），
   // 并加入到已有的mappedBehaviors集合中。
-  List<MappedBehavior> mapAndAdd(List<MappedBehavior> mappedBehaviors, Behavior behavior) {
+  List<MappedBehavior> mapAndAdd(
+    List<MappedBehavior> mappedBehaviors,
+    Behavior behavior,
+  ) {
     bool noTurnInfo = true;
     int catchTurn = behavior.turn;
     behavior.turn = null;
@@ -35,11 +37,34 @@ class BehaviorController {
   }
 
   // 将每一次记录的玩家Behavior
-  List<IndividualRecord> groupedBehaviorValues(List<Behavior> behaviors) {
+  Map<String, IndividualRecord> groupedBehaviorValues(
+    Map<String, IndividualRecord> individualRecords,
+    List<Behavior> behaviors,
+  ) {
     int playerNumber = 0;
 
-    return List.generate(playerNumber, generator);
+    for (Behavior bh in behaviors) {
+      if (individualRecords.containsKey(bh.player)) {
+        var currentPersonRecord = individualRecords[bh.player];
+
+        // turnRecords
+        bool noTurnInfo = true;
+        for (TurnRecord eachTR in currentPersonRecord.turnRecords) {
+          if (eachTR.turn == bh.turn) {
+            eachTR.behaviors.add(bh);
+            noTurnInfo = false;
+          }
+        }
+        if (noTurnInfo) {
+          TurnRecord newTurnRecord =
+              new TurnRecord(turn: bh.turn, behaviors: [bh]);
+        }
+
+        // behaviorRecords
+
+      }
+    }
+
+    return List.generate(playerNumber, (index) {});
   }
-
-
 }
