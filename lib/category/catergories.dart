3e625eba0cf4d64aa9bfd72de338f2094ba42019
Dafei Ning <ningdafei@gmail.com>
2019-12-30 20:45:02 -0800

    category item map to categories

--- lib/category/catergories.dart
+++ lib/category/catergories.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:werewolf_quantjudge/models/category.dart';
+import 'categoryItem.dart';
+
+import '../data/simulatedData.dart';
 
 class CatergoriesScreen extends StatelessWidget {
+  List<Category> categories = SimulateData().categories;
+
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[
-        
-      ],
+      children: categories
+          .map((catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ))
+          .toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
