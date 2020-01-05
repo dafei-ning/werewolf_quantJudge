45c8f1bad0350a56498c6a02ea90dd43964fef92
Dafei Ning <ningdafei@gmail.com>
2020-01-05 11:12:12 -0800

    4 page navi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:werewolf_quantjudge/2.meetUp/meetUpHomePage.dart';
+import 'package:werewolf_quantjudge/3.gameJudge/judgeHomePage.dart';
+import 'package:werewolf_quantjudge/4.scoreList/scoreListHomePage.dart';
 
 import '0.category/catergories.dart';
 import '1.behavior/behaviorHomePage.dart';
@@ -54,6 +57,9 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CatergoriesScreen('湾区狼人杀'),
         BehaviorHomePage.routeName: (ctx) => BehaviorHomePage('狼人杀玩家行为记录'),
+        MeetUpHomePage.routeName: (ctx) => MeetUpHomePage('湾区狼人杀组局'),
+        JudgeHomePage.routeName: (ctx) => JudgeHomePage('狼人杀小法官'),
+        ScoreListHomePage.routeName: (ctx) => ScoreListHomePage('湾区狼人杀英雄榜'),
       },
     );
   }
