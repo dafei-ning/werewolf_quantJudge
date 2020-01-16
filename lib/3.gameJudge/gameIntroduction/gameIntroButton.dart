ea2913176bd59e3afc254610f38a7df51840b0ad
Dafei Ning <ningdafei@gmail.com>
2020-01-15 18:47:00 -0800

    game intro link fixed

--- lib/3.gameJudge/gameIntroduction/gameIntroButton.dart
+++ lib/3.gameJudge/gameIntroduction/gameIntroButton.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class GameIntroButton extends StatelessWidget {
   final String title = "GameIntroduction";
+  final Color gameIntroButtonColor = Colors.teal[500];
 
   void selectJudgeCategory(BuildContext ctx) {
     print(ctx);
@@ -10,12 +12,38 @@ class GameIntroButton extends StatelessWidget {
       arguments: {'title': title},
     );
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(30),
+    var gaIntroBuIOS = FlatButton(
+      padding: const EdgeInsets.all(25),
+      onPressed: () => selectJudgeCategory(context),
+      color: gameIntroButtonColor,
       child: Text(title),
-      color: Colors.teal[300],
+    );
+    var gaIntroBuAndroid = InkWell(
+      onTap: () => selectJudgeCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(5),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(title),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              gameIntroButtonColor.withOpacity(0.7),
+              gameIntroButtonColor,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+
+    return Container(
+      child: Platform.isIOS ? gaIntroBuIOS : gaIntroBuAndroid,
     );
   }
 }
