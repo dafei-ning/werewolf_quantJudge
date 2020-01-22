3faed483397cc4612808593940c59eafe8206007
Dafei Ning <ningdafei@gmail.com>
2020-01-21 19:02:58 -0800

    playerAmount choose structure

--- lib/3.gameJudge/3.2.createRoom/0.playerAmountChoose/playerAmountChoose.dart
+++ lib/3.gameJudge/3.2.createRoom/0.playerAmountChoose/playerAmountChoose.dart
@@ -1,10 +1,41 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PlayerAmountChoose extends StatelessWidget {
+class PlayerAmountChoose extends StatefulWidget {
+  @override
+  _PlayerAmountChooseState createState() => _PlayerAmountChooseState();
+}
+
+class _PlayerAmountChooseState extends State<PlayerAmountChoose> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('15人局'),
+    int selectItem = 1;
+
+    Future<String> _choosePlayerAmount() {
+      return showCupertinoModalPopup(
+          context: context,
+          builder: (ctx) {
+            return SizedBox(
+              height: 300,
+              child: CupertinoPicker(
+                itemExtent: 40,
+                onSelectedItemChanged: (int index) {
+                  selectItem = index;
+                },
+                children: <Widget>[
+                  Text("1"),
+                  Text("2"),
+                  Text("3"),
+                  Text("4"),
+                ],
+              ),
+            );
+          });
+    }
+
+    return RaisedButton(
+      onPressed: _choosePlayerAmount,
+      child: Text('sss ${selectItem}'),
     );
   }
 }
