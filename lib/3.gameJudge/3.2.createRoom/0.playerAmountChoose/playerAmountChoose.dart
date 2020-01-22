a6f4048038e79d824c37d05dde04b434993c7780
Dafei Ning <ningdafei@gmail.com>
2020-01-21 20:32:30 -0800

    playerAmount choose structure demo copied

--- lib/3.gameJudge/3.2.createRoom/0.playerAmountChoose/playerAmountChoose.dart
+++ lib/3.gameJudge/3.2.createRoom/0.playerAmountChoose/playerAmountChoose.dart
@@ -1,48 +1,68 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const List<String> colors = const <String>[
+  'Red',
+  'Yellow',
+  'Amber',
+  'Blue',
+  'Black',
+  'Pink',
+  'Purple',
+  'White',
+  'Grey',
+  'Green',
+];
+
 class PlayerAmountChoose extends StatefulWidget {
   @override
   _PlayerAmountChooseState createState() => _PlayerAmountChooseState();
 }
 
 class _PlayerAmountChooseState extends State<PlayerAmountChoose> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
 
-    String _selectedAmount;
-
-    Future<String> _choosePlayerAmount() {
-      return showCupertinoModalPopup(
-          context: context,
-          builder: (ctx) {
-            return SizedBox(
-              height: 300,
-              child: CupertinoPicker(
-                looping: true,
-                itemExtent: 40,
-                onSelectedItemChanged: (value) {
-                  setState(() {
-                    _selectedAmount = value.toString();
-                    print(' print selected: ${_selectedAmount}');
-                  });
-                },
-                children: <Widget>[
-                  Text("1"),
-                  Text("2"),
-                  Text("3"),
-                  Text("4"),
-                ],
-              ),
-            );
-          });
-    }
-
-    return RaisedButton(
-      onPressed: _choosePlayerAmount,
-      child: Text(
-        _selectedAmount == null ? '选择本局人数' : '人数: ${_selectedAmount}' 
+    var playerAmountDialog = Container(
+      child: Row(
+        children: <Widget>[
+          CupertinoButton(
+              child: Text("Select Color :"),
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Container(
+                        height: 200.0,
+                        child: CupertinoPicker(
+                            itemExtent: 32.0,
+                            onSelectedItemChanged: (int index) {
+                              setState(() {
+                                _selectedIndex = index;
+                              });
+                            },
+                            children: new List<Widget>.generate(colors.length,
+                                (int index) {
+                              return new Center(
+                                child: new Text(colors[index]),
+                              );
+                            })),
+                      );
+                    });
+              }),
+          Text(
+            colors[_selectedIndex],
+            style: TextStyle(fontSize: 18.0),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+        ],
       ),
     );
+
+    return playerAmountDialog;
   }
 }
