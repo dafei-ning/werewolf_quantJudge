bd523fa6984dcc7576a52d4fc70e3fec27216a9e
Dafei Ning <ningdafei@gmail.com>
2020-01-28 17:29:03 -0800

    modify

--- lib/3.gameJudge/3.2.createRoom/1.teamConfigChoose/teamConfigChoose.dart
+++ lib/3.gameJudge/3.2.createRoom/1.teamConfigChoose/teamConfigChoose.dart
@@ -12,16 +12,46 @@ class TeamConfigChoose extends StatefulWidget {
 
 class _TeamConfigChooseState extends State<TeamConfigChoose> {
   static List<AmountConfiguration> acs = WolfDataBase().amountConfigurations;
-  
+  static Map<GamePattern, String> patternMap  = WolfDataBase().patternMap;
+
+  String selectedGcfgName;
+
+  List<Widget> _gameConfigChipList(AmountConfiguration ac) {
+    List<Widget> resultList = [];
+    ac.charConfigs.forEach((k, v) {
+      String cfgName = patternMap[k];
+      var gameConfigChip = Container(
+        child: ChoiceChip(
+          label: Text(k.toString()),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          selected: selectedGcfgName == cfgName,
+            onSelected: (selected) {
+              setState(() {
+                selectedGcfgName = cfgName;
+                //widget.onSelectionChanged(selectedTag);
+              });
+            },
+        ),
+      );
+      resultList.add(gameConfigChip);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    AmountConfiguration ac = widget.playerAmount == 0? null : acs.singleWhere((ac) => ac.playerAmount == widget.playerAmount);
+    AmountConfiguration ac = widget.playerAmount == 0
+        ? null
+        : acs.singleWhere((ac) => ac.playerAmount == widget.playerAmount);
 
-    var textss = ac == null? '第一行，选人数' : '第一行，版型 ${ac.playerAmount.toString()}';
+    var gameCFGChips = ac == null ? null : _gameConfigChipList(ac);
+
+    var textss =
+        ac == null ? '第一行，选人数' : '第一行，版型 ${ac.playerAmount.toString()}';
 
     return Container(
-      child: Text(textss),
+      child: gameCFGChips == null? Text('第一行，选人数'): Wrap(children: gameCFGChips)
     );
   }
 }
