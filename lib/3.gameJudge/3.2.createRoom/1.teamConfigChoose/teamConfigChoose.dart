1a5750b7008e8f0d42b4d5b8ec6715de933190ba
Dafei Ning <ningdafei@gmail.com>
2020-01-28 18:49:46 -0800

    pass amount for configs chips strucuttre done

--- lib/3.gameJudge/3.2.createRoom/1.teamConfigChoose/teamConfigChoose.dart
+++ lib/3.gameJudge/3.2.createRoom/1.teamConfigChoose/teamConfigChoose.dart
@@ -12,17 +12,17 @@ class TeamConfigChoose extends StatefulWidget {
 
 class _TeamConfigChooseState extends State<TeamConfigChoose> {
   static List<AmountConfiguration> acs = WolfDataBase().amountConfigurations;
-  static Map<GamePattern, String> patternMap  = WolfDataBase().patternMap;
+  static Map<GamePattern, String> patternMap = WolfDataBase().patternMap;
 
   String selectedGcfgName;
-
   List<Widget> _gameConfigChipList(AmountConfiguration ac) {
     List<Widget> resultList = [];
     ac.charConfigs.forEach((k, v) {
       String cfgName = patternMap[k];
       var gameConfigChip = Container(
+        margin: EdgeInsets.all(5),
         child: ChoiceChip(
-          label: Text(k.toString()),
+          label: Text(cfgName),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(5),
           ),
@@ -37,6 +37,7 @@ class _TeamConfigChooseState extends State<TeamConfigChoose> {
       );
       resultList.add(gameConfigChip);
     });
+    return resultList;
   }
 
   @override
@@ -44,11 +45,10 @@ class _TeamConfigChooseState extends State<TeamConfigChoose> {
     AmountConfiguration ac = widget.playerAmount == 0
         ? null
         : acs.singleWhere((ac) => ac.playerAmount == widget.playerAmount);
-
     var gameCFGChips = ac == null ? null : _gameConfigChipList(ac);
-
     return Container(
-      child: gameCFGChips == null? Text('第一行，选人数'): Wrap(children: gameCFGChips)
-    );
+        child: gameCFGChips == null
+            ? Text('请选择参与游戏的人数')
+            : Wrap(children: gameCFGChips));
   }
 }
