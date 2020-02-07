46b24f7f5ef980c0a601db2dfc980d59782940fd
Dafei Ning <ningdafei@gmail.com>
2020-02-06 19:44:48 -0800

    characters struct 2

--- lib/3.gameJudge/3.6.gameRoom/characters/specials/1.seer.dart
+++ lib/3.gameJudge/3.6.gameRoom/characters/specials/1.seer.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-class Seer extends StatelessWidget {
+class Seer extends StatefulWidget {
+  final List<int> playerList;
+  Seer(this.playerList);
+
+  @override
+  _SeerState createState() => _SeerState();
+}
+
+class _SeerState extends State<Seer> {
+  void _checkOnePlayer() {
+
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Container();
---
+++ lib/models/inGameModel.dart
@@ -0,0 +1,3 @@
+class CharacterGroup {
+  
+}
