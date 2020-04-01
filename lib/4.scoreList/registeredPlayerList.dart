f54b5c55a8d24f1128f3f9cd63d24825a3e85753
Dafei Ning <ningdafei@gmail.com>
2020-04-01 04:58:14 -0700

    create registered player list

---
+++ lib/4.scoreList/registeredPlayerList.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RegisterPlayerList extends StatefulWidget {
+  @override
+  _RegisterPlayerListState createState() => _RegisterPlayerListState();
+}
+
+class _RegisterPlayerListState extends State<RegisterPlayerList> {
+
+  final _registeredPlayerList = <String>[];
+  final _fontStyle = const TextStyle(fontSize: 15);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      
+    );
+  }
+}
